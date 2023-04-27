5d8d8dc108e85beac1276422d1529cf66cb64a69
RoderickMMM <rodrigomfoggia84@gmail.com>
2023-04-27 05:00:59 -0300

    Initial Commit

---
+++ Pages/Dinosaurs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TESTPROYECT_PRACTICE1.Pages
+{
+    public class DinosaursModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/History.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TESTPROYECT_PRACTICE1.Pages
+{
+    public class HistoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
